5056c3f051d30a33f0e795947b4c0bd4e5189fcd
ElsLommelen <els.lommelen@inbo.be>
2026-01-13 21:12:53 +0100

    grafieken per reservaat (autoqmd) stap 3: maak pre-render script

---
+++ source/website/pre_render.R
@@ -0,0 +1,14 @@
+library(flandersqmd)
+
+plotinfo <- read.csv2('../../data/plotinfo.csv', header = TRUE, dec=',', sep=';')
+reserves <- unique(plotinfo$forest_reserve)
+
+# Create child document for each reserve
+# Include child files in report
+autoqmd_prepare(
+  selected_reserve = reserves,
+  label = gsub("\\s", ".", tolower(reserves)),
+  template = "fiche_reservaat.qmd",
+  child_dir = "fiches_reservaat_qmd",
+  qmd_file = "grafieken_per_reservaat.qmd"
+)
