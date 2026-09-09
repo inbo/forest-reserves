e6ad884858a63238fdb3e6c0217a7eeab317d4e0
THOMAES <arno.thomaes@inbo.be>
2026-09-09 12:02:16 +0200

    aangepast zodat reservaten met enkel kernvlakte ander script gebruikt dan diegene met cirkelplots

--- source/website/pre_render.R
+++ source/website/pre_render.R
@@ -2,13 +2,24 @@ library(flandersqmd)
 library(knitr)
 library(quarto)
 library(purrr)
+library(dplyr)
+library(stringr)
 
 plotinfo <- read.csv2('../../data/plotinfo.csv', header = TRUE, dec = ',', sep = ';')
 metadata <- read.csv2('../../data/metadata.csv', header = TRUE, dec = ',', sep = ';')
 
-reserves <- sort(unique(plotinfo$forest_reserve))
+# Kerselaerspleyn samenvoegen bij Zoniën en volgorde aanpassen zoals in metadata
+plotinfo$forest_reserve <- ifelse(plotinfo$forest_reserve == "Kersselaerspleyn", "Zwaenepoel", plotinfo$forest_reserve)
+plotinfo2 <- left_join(plotinfo, metadata, by = "forest_reserve")
+plotinfo2 <- arrange(plotinfo2, Nr)
+
+reserves <- unique(plotinfo2$forest_reserve)
 reserves_label <- gsub("\\s", "_", tolower(reserves))
-reserves_correct <- metadata[metadata$forest_reserve %in% reserves, "Correct_name"]
+reserves_correct <- metadata[metadata$forest_reserve %in% reserves,]$Correct_name
+
+reserves_kort <- metadata$forest_reserve[!(metadata$forest_reserve %in% reserves)]
+reserves_label_kort <- gsub("\\s", "_", tolower(reserves_kort))
+reserves_kort_correct <- metadata[metadata$forest_reserve %in% reserves_kort,]$Correct_name
 
 # Generate child documents
 out_files <- autoqmd_generate_children(
@@ -18,6 +29,20 @@ out_files <- autoqmd_generate_children(
   child_dir = "fiches_reservaat_qmd",
   freeze = "label"
 )
+
+# Generate child documents
+out_files_kort <- autoqmd_generate_children(
+  selected_reserve = reserves_kort,
+  label = reserves_label_kort,
+  template = "_fiche_reservaat_kort.qmd",
+  child_dir = "fiches_reservaat_qmd",
+  freeze = "label"
+)
+
+out_files <- c(out_files, out_files_kort)
+reserves_correct <- c(reserves_correct, reserves_kort_correct)
+out_files <- out_files[str_order(reserves_correct)]
+reserves_correct <- sort(reserves_correct)
 
 # insert entries into the Quarto configuration
 autoqmd_insert_children(
